7f625f736ee37218f16e05c93ecec5ec5ce4878c
Hiroya Onoe <git@onoe.dev>
2023-12-17 23:02:49 +0900

    Update restart.sh

--- dsb-hr/restart.sh
+++ dsb-hr/restart.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -eux
 
-# dsb-hr namespaceのdeploymentのname一覧を取得
-deployments=$(kubectl get deployments -n dsb-hr -o jsonpath='{.items[*].metadata.name}')
+# # dsb-hr namespaceのdeploymentのname一覧を取得
+# deployments=$(kubectl get deployments -n dsb-hr -o jsonpath='{.items[*].metadata.name}')
 
-# 取得したdeploymentそれぞれでkubectl rollout restartコマンドを実行
-for deployment in $deployments
-do
-    kubectl rollout restart deployment/$deployment -n dsb-hr
-    echo "Restarted deployment: $deployment"
-done
+# # 取得したdeploymentそれぞれでkubectl rollout restartコマンドを実行
+# for deployment in $deployments
+# do
+#     kubectl rollout restart deployment/$deployment -n dsb-hr
+#     echo "Restarted deployment: $deployment"
+# done
+
+kubectl delete -n dsb-hr -Rf ../../DeathStarBench/hotelReservation/kubernetes
+kubectl apply -n dsb-hr -Rf ../../DeathStarBench/hotelReservation/kubernetes
