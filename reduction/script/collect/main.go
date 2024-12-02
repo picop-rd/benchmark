be63177c49f92324d36f7bac8a39b3e57605521d
Hiroya Onoe <hiroya@onoe.dev>
2024-12-02 20:41:01 +0900

    cp

---
+++ reduction/script/collect/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	dir := flag.String("dir", "", "directory to save data")
+	name := flag.String("name", "", "project name")
+	timestamp := flag.String("timestamp", "", "timestamp to exec command (RFC3339)")
+	interval := flag.Int("interval", 10, "interval to exec command (second)")
+	duration := flag.Int("duration", 300, "duration to exec command (second)")
+	flag.Parse()
+	cmd := flag.Args()
+
+	fmt.Printf("dir: %s\n", *dir)
+	fmt.Printf("name: %s\n", *name)
+	fmt.Printf("timestamp: %s\n", *timestamp)
+	fmt.Printf("interval: %d\n", *interval)
+	fmt.Printf("duration: %d\n", *duration)
+	fmt.Printf("cmd: %s\n", strings.Join(cmd, " "))
+
+	if len(*dir) == 0 || len(*name) == 0 || *interval == 0 || *duration == 0 || len(cmd) < 2 || len(*timestamp) == 0 {
+		flag.Usage()
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, os.Interrupt)
+		<-quit
+		cancel()
+	}()
+
+	dirname := filepath.Join(*dir, *name, *timestamp)
+	if _, err := os.Stat(dirname); err == nil {
+		fmt.Printf("duplicated timestamp: %s\n", dirname)
+		return
+	}
+	err := os.MkdirAll(dirname, 0755)
+	if err != nil {
+		fmt.Printf("mkdir error: %v\n", err)
+		return
+	}
+	log.Printf("created: %s\n", dirname)
+
+	collect(ctx, dirname, cmd, *interval, *duration)
+	log.Println("completed")
+}
+
+func collect(ctx context.Context, dirname string, cmd []string, interval, duration int) {
+	var wg sync.WaitGroup
+	after := time.After(time.Duration(duration) * time.Second)
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	for cnt := 0; ; cnt++{
+		select {
+		case <-ctx.Done():
+			return
+		case <-after:
+			log.Println("waiting")
+			wg.Wait()
+			return
+		case <-ticker.C:
+			wg.Add(1)
+			go func(cnt int) {
+				log.Printf("exec command: %d\n", cnt)
+				err := execCmd(dirname, cmd)
+				if err != nil {
+					fmt.Printf("exec command %d error: %v\n", cnt, err)
+				}
+				wg.Done()
+				log.Printf("finished: %d\n", cnt)
+			}(cnt)
+		}
+	}
+}
+
+func execCmd(dirname string, cmd []string) error {
+	now := time.Now().Unix()
+	c := exec.Command(cmd[0], cmd[1:]...)
+	bytes, err := c.Output()
+	c.Stderr = os.Stderr
+	if err != nil {
+		return err
+	}
+	file := filepath.Join(dirname, fmt.Sprintf("%d", now))
+	err = os.WriteFile(file, bytes, 0644)
+	if err != nil {
+		return err
+	}
+	log.Printf("saved file: %s\n", file)
+	return nil
+}
---
+++ reduction/script/parse/main.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const podPrefixLength = len("-6fd889d5dd-fnkpv")
+
+var (
+	orgNames = []string{"prox", "ingress", "mongodb", "memcached"}
+)
+
+type PodUsage struct {
+	Data map[int]int
+}
+
+func main() {
+	name := flag.String("name", "", "project name")
+	timestamp := flag.String("timestamp", "", "timestamp to exec command (RFC3339)")
+	input := flag.String("input", "", "input dir path")
+	output := flag.String("output", "", "output dir path")
+	container := flag.Bool("containers", false, "container flag")
+	flag.Parse()
+
+	if len(*input) == 0 || len(*output) == 0 || len(*name) == 0 || len(*timestamp) == 0 {
+		flag.Usage()
+		return
+	}
+
+	fmt.Printf("name: %s\n", *name)
+	fmt.Printf("timestamp: %s\n", *timestamp)
+	fmt.Printf("input dir path: %s\n", *input)
+	fmt.Printf("output dir path: %s\n", *output)
+
+	// Directory where the .txt files are stored
+	inputDir := filepath.Join(*input, *name, *timestamp)
+	outputDir := filepath.Join(*output, *name, *timestamp)
+
+	// Maps to store pod usages
+	cpuUsage := make(map[string]*PodUsage)
+	memoryUsage := make(map[string]*PodUsage)
+
+	// Process each file in the directory
+	filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			unixTime, err := strconv.Atoi(info.Name())
+			if err != nil {
+				return err
+			}
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			log.Printf("processing: %s\n", path)
+			processFile(file, unixTime, cpuUsage, memoryUsage, *container)
+		}
+		return nil
+	})
+
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		fmt.Printf("mkdir error: %v\n", err)
+		return
+	}
+	log.Printf("created: %s\n", outputDir)
+	// Export to Raw CSV
+	rawDir := filepath.Join(outputDir, "raw")
+	err = os.MkdirAll(rawDir, 0755)
+	if err != nil {
+		fmt.Printf("mkdir error: %v\n", err)
+		return
+	}
+	log.Printf("created: %s\n", rawDir)
+	exportToRawCSV(cpuUsage, filepath.Join(rawDir, "cpu.csv"))
+	log.Printf("exported: %s\n", filepath.Join(rawDir, "cpu.csv"))
+	exportToRawCSV(memoryUsage, filepath.Join(rawDir, "memory.csv"))
+	log.Printf("exported: %s\n", filepath.Join(rawDir, "memory.csv"))
+
+	// Export to Org CSV
+	orgDir := filepath.Join(outputDir, "org")
+	err = os.MkdirAll(orgDir, 0755)
+	if err != nil {
+		fmt.Printf("mkdir error: %v\n", err)
+		return
+	}
+	log.Printf("created: %s\n", orgDir)
+	exportToOrgTxt(cpuUsage, filepath.Join(orgDir, "cpu.txt"))
+	log.Printf("exported: %s\n", filepath.Join(orgDir, "cpu.txt"))
+	exportToOrgTxt(memoryUsage, filepath.Join(orgDir, "memory.txt"))
+	log.Printf("exported: %s\n", filepath.Join(orgDir, "memory.txt"))
+	exportToOrgCsv(cpuUsage, filepath.Join(orgDir, "cpu.csv"))
+	log.Printf("exported: %s\n", filepath.Join(orgDir, "cpu.csv"))
+	exportToOrgCsv(memoryUsage, filepath.Join(orgDir, "memory.csv"))
+	log.Printf("exported: %s\n", filepath.Join(orgDir, "memory.csv"))
+}
+
+func processFile(file *os.File, unixTime int, cpu, memory map[string]*PodUsage, container bool) {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "CPU(cores)") {
+			continue // Skip header line
+		}
+		parts := strings.Fields(line)
+		if len(parts) < 3 {
+			fmt.Printf("Invalid line: %s\n", line)
+			continue // Invalid line
+		}
+
+		podName, err := converPodName(parts[0])
+		if err != nil {
+			fmt.Printf("Invalid pod name: %s error: %s\n", parts[0], err)
+			continue // Invalid pod name
+		}
+		if container {
+			podName, err = convertContainerName(podName, parts[1])
+			if err != nil {
+				fmt.Printf("Invalid pod name: %s %s error: %s\n", podName, parts[1], err)
+				continue // Invalid container name
+			}
+		}
+
+		var rawcpuValue string
+		if container {
+			rawcpuValue = parts[2]
+		} else {
+			rawcpuValue = parts[1]
+		}
+		cpuValue, err := convertCPUValue(rawcpuValue)
+		if err != nil {
+			fmt.Printf("Invalid CPU value: %s error: %s\n", rawcpuValue, err)
+			continue // Invalid CPU value
+		}
+		var rawmemoryValue string
+		if container {
+			rawmemoryValue = parts[3]
+		} else {
+			rawmemoryValue = parts[2]
+		}
+		memoryValue, err := convertMemoryValue(rawmemoryValue)
+		if err != nil {
+			fmt.Printf("Invalid memory value: %s error: %s\n", rawmemoryValue, err)
+			continue // Invalid memory value
+		}
+
+		if _, exists := cpu[podName]; !exists {
+			cpu[podName] = &PodUsage{Data: make(map[int]int)}
+		}
+		cpu[podName].Data[unixTime] = cpuValue
+
+		if _, exists := memory[podName]; !exists {
+			memory[podName] = &PodUsage{Data: make(map[int]int)}
+		}
+		memory[podName].Data[unixTime] = memoryValue
+	}
+}
+
+func converPodName(raw string) (string, error) {
+	if len(raw) < podPrefixLength {
+		return "", fmt.Errorf("invalid pod name length: %s", raw)
+	}
+	return raw[:len(raw)-podPrefixLength], nil
+}
+
+func convertContainerName(pod, raw string) (string, error) {
+	pod = strings.Replace(pod, "memcached", "xmc", 1)
+	pod = strings.Replace(pod, "mongodb", "xmo", 1)
+	if strings.Contains(raw, "proxy") {
+		return fmt.Sprintf("prox-%s", pod), nil
+	} else if strings.Contains(raw, "ingress") {
+		return fmt.Sprintf("ingress-%s", pod), nil
+	} else if strings.Contains(raw, "mongo") {
+		return fmt.Sprintf("mongodb-%s", pod), nil
+	} else if strings.Contains(raw, "mmc") {
+		return fmt.Sprintf("memcached-%s", pod), nil
+	} else {
+		return fmt.Sprintf("app-%s", pod), nil
+	}
+}
+
+func convertCPUValue(raw string) (int, error) {
+	switch raw[len(raw)-1:] {
+	case "m":
+		return strconv.Atoi(raw[:len(raw)-1])
+	default:
+		return 0, fmt.Errorf("invalid CPU value: %s", raw)
+	}
+}
+
+func convertMemoryValue(raw string) (int, error) {
+	switch raw[len(raw)-2:] {
+	case "Mi":
+		return strconv.Atoi(raw[:len(raw)-2])
+	default:
+		return 0, fmt.Errorf("invalid memory value: %s", raw)
+	}
+}
+
+func exportToRawCSV(data map[string]*PodUsage, filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating CSV file:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	length := 0
+	isFirst := true
+	for podName, usage := range data {
+		if isFirst {
+			isFirst = false
+			length = len(usage.Data) + 1
+			// Write headers
+			headers := make([]string, 0, length)
+			headers = append(headers, "name")
+			// Add Unix timestamps as headers
+			for unixTime := range usage.Data {
+				headers = append(headers, strconv.Itoa(unixTime))
+			}
+			writer.Write(headers)
+		}
+		row := make([]string, 0, length)
+		row = append(row, podName)
+		for _, value := range usage.Data {
+			row = append(row, strconv.Itoa(value))
+		}
+		writer.Write(row)
+	}
+}
+
+func exportToOrgTxt(data map[string]*PodUsage, filename string) {
+	res := 0.0
+	for _, usage := range data {
+		ints := make([]int, 0, len(usage.Data))
+		for _, value := range usage.Data {
+			ints = append(ints, value)
+		}
+		avg := calculateAverage(ints)
+		res += avg
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating Txt file:", err)
+		return
+	}
+	defer file.Close()
+
+	file.Write([]byte(fmt.Sprintf("%f", res)))
+}
+
+func exportToOrgCsv(data map[string]*PodUsage, filename string) {
+	orgData := make(map[string]float64, len(orgNames))
+	for _, name := range orgNames {
+		orgData[name] = 0.0
+	}
+	for name, usage := range data {
+		ints := make([]int, 0, len(usage.Data))
+		for _, value := range usage.Data {
+			ints = append(ints, value)
+		}
+		yet := true
+		for _, orgName := range orgNames {
+			if strings.Contains(name, orgName) {
+				orgData[orgName] += calculateAverage(ints)
+				yet = false
+				break
+			}
+		}
+		if yet {
+			orgData["app"] += calculateAverage(ints)
+		}
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating Csv file:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for name, value := range orgData {
+		writer.Write([]string{name, fmt.Sprintf("%f", value)})
+	}
+}
+
+func calculateAverage(values []int) float64 {
+	sort.Ints(values)
+	trimmedValues := values[len(values)/20 : 19*len(values)/20] // 上位下位5%を除外
+	var sum int
+	for _, v := range trimmedValues {
+		sum += v
+	}
+	return float64(sum) / float64(len(trimmedValues))
+}
